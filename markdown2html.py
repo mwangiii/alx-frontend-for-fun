0ef17d9bc55be75022c64e5a6e2580ac078e7573
mwangiii <mwangi8kevin@gmail.com>
2023-10-17 15:46:47 +0300

    converting markdown tags to html tags

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,8 @@
 """
 import sys
 import os.path
+import re
+
 
 if __name__ == '__main__':
     arg = sys.argv
@@ -22,5 +24,21 @@ if __name__ == '__main__':
     if not os.path.isfile(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
+
+  # Markdown headings to HTML tags
+    with open("README.md", mode="r") as f:
+        content = f.read()
+
+    # I used regex to look for the '#'
+    markdown_headings = re.compile(r"^(#{1,6})\s+(.*)$", flags=re.MULTILINE)
+
+    # I used re.sub with a lambda function to replace all Markdown headings with  HTML headings
+    html_content = markdown_headings.sub(
+        lambda match: f"<h{len(match.group(1))}>{match.group(2)}</h{len(match.group(1))}>",
+        content,
+    )
+    # Write the HTML content to the output file
+    with open(output_file_name, mode="w") as f:
+        f.write(html_content)
 
     sys.exit(0)
