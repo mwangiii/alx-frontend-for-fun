438aee26851f7a406d2b8e80c6d792f5ecc3c17a
mwangiii <mwangi8kevin@gmail.com>
2023-10-17 12:14:03 +0300

    finxing markdown file

--- markdown2html.py
+++ markdown2html.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
 import sys
-import os
+import os.path
 
+if __name__ == '__main__':
+    arg = sys.argv
 
-def convert_markdown_to_html(markdown_file, output_file):
-    """Converts Markdown to HTML"""
-
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+    if len(arg) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
-    elif not os.path.exists(markdown_file):
+
+    markdown_file = arg[1]
+    output_file_name = arg[2]
+
+    if not os.path.isfile(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
-    else:
-        # Actual conversion logic goes here
-        print("Conversion successful!")
-        sys.exit(0)
+
+    sys.exit(0)
